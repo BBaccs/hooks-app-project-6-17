2006d066a48fffe2ac498de8dff01bcda1195bc5
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-06-19 11:43:37 -0400

    Update EpisodeCard.js

--- src/components/EpisodeCard.js
+++ src/components/EpisodeCard.js
@@ -9,6 +9,7 @@ function EpisodeCard({ data }) {
   async function fetchData(urlArr, cardId) {
     try {
       const promises = urlArr.map(url => fetch(url).then(response => {
+        console.log(url)
         if (!response.ok) {
           throw new Error('Network response was not ok: ' + response.statusText);
         }
@@ -23,60 +24,56 @@ function EpisodeCard({ data }) {
     }
   }
 
-const handleClick = (id) => {
-  setShowNames(prevData => ({
-    ...prevData,
-    [id]: !prevData[id]
-  }));
-  // console.log(showNames[id], id)
-};
+  const handleClick = (id) => {
+    setShowNames(prevData => ({
+      ...prevData,
+      [id]: !prevData[id]
+    }));
+  };
 
-function cleanImageUrl(url) {
-  console.log(url)
-  const indexOfPng = url.indexOf('.png');
-  if (indexOfPng !== -1) {
-    // Add 4 because `.png` has 4 characters
-    return url.substring(0, indexOfPng + 4);
+  function cleanImageUrl(url) {
+    if (!url) return '/logo512.png'; // Add a placeholder img here
+    const indexOfPng = url.indexOf('.png');
+    if (indexOfPng !== -1) {
+      return url.substring(0, indexOfPng + 4);
+    }
+    return url;
   }
-  return url; // Return the original URL if .png is not found
-}
 
+  return (
+    <Card key={data.id} sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <CardMedia
+          component="img"
+          alt=''
+          height="140"
+          image={cleanImageUrl(data.img)}
+        />
+        <Typography gutterBottom variant="h5" component="div">
+          <b>Title:</b> {data.name}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          <b>Episode:</b> {data.episode}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button onClick={() => fetchData(data.characters, data.id)} size="large">{showNames[data.id] ? 'hide' : 'show'} Characters List</Button>
 
-
-return (
-  <Card key={data.id} sx={{ maxWidth: 345 }}>
-    <CardContent>
-      <CardMedia
-        component="img"
-        alt={data.name}
-        height="140"
-        image={cleanImageUrl(data.img)}
-      />
-      <Typography gutterBottom variant="h5" component="div">
-        <b>Title:</b> {data.name}
-      </Typography>
-      <Typography gutterBottom variant="h5" component="div">
-        <b>Episode:</b> {data.episode}
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button onClick={() => fetchData(data.characters, data.id)} size="large">{showNames[data.id] ? 'hide' : 'show'} Characters List</Button>
-
-    </CardActions>
-    <>
-      <ul id={data.id} className={showNames[data.id] ? 'show' : 'hide'}> 
-        {characterData.map((char, index) =>
-          <li className="mb-2" key={index}>
-            <span><b>Name:</b> {char.name}</span>
-            <span style={{ display: 'block' }}>
-              {char.age === 'unknown' || !char.age ? '' : <><b>Age:</b> {char.age}</>}
-            </span>
-          </li>
-        )}
-      </ul>
-    </>
-  </Card>
-);
+      </CardActions>
+      <>
+        <ul id={data.id} className={showNames[data.id] ? 'show' : 'hide'}>
+          {characterData.map((char, index) =>
+            <li className="mb-2" key={index}>
+              <span><b>Name:</b> {char.name}</span>
+              <span style={{ display: 'block' }}>
+                {char.age === 'unknown' || !char.age ? '' : <><b>Age:</b> {char.age}</>}
+              </span>
+            </li>
+          )}
+        </ul>
+      </>
+    </Card>
+  );
 }
 
 export default EpisodeCard;
