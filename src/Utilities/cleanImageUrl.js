362f478d081a73d2b196971c2d0a084085720084
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-06-19 13:11:28 -0400

    Update cleanImageUrl.js

--- src/Utilities/cleanImageUrl.js
+++ src/Utilities/cleanImageUrl.js
@@ -1,5 +1,5 @@
 export default function cleanImageUrl(url) {
-    if (!url) return '/placeholder.webp'; // Add a placeholder img here
+    if (!url || url === 'unknown') return '/placeholder.webp'; // Add a placeholder img here
     const indexOfPng = url.indexOf('.png');
     if (indexOfPng !== -1) {
       return url.substring(0, indexOfPng + 4);
