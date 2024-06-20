203aff84ba763d2603b14defdaaae68e6cde6da1
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-06-20 17:56:49 -0400

    part 2 update charDataFetcher hook

--- src/hooks/useCharacterDataFetcher.js
+++ src/hooks/useCharacterDataFetcher.js
@@ -5,27 +5,31 @@ export default function useCharacterDataFetcher() {
     const [characterData, setCharacterData] = useState({ current: [], former: [], notable: [] });
 
     async function fetchData(urlData, cardId, charType) {
+        // Ensure urlData is an array, and filter out any item that doesn't start with 'http'
         let urlArr = Array.isArray(urlData) ? urlData : urlData.split(",");
+        urlArr = urlArr.map(url => url.trim()).filter(url => url.startsWith('https'));
         try {
             const promises = urlArr.map(url => fetch(url).then(response => {
                 if (!response.ok) {
-                    throw new Error('Network response was not ok: ' + response.statusText);
+                    console.error(`Error fetching ${url}: ${response.statusText}`);
+                    throw new Error(`Network response was not ok: ${response.statusText}`);
                 }
                 return response.json();
             }));
+
             const results = await Promise.all(promises);
-            if (charType === 'current') {
-                setCharacterData(prevProps => ({ ...prevProps, current: results, former: prevProps.former }));
-            } else if (charType === 'former') {
-                setCharacterData(prevProps => ({ ...prevProps, current: prevProps.current, former: results }));
-            } else if (charType === 'notable') {
-                // If charType is 'none', it will update both current and former
-                setCharacterData(prevProps => ({ ...prevProps, current: prevProps.current, former: prevProps.current, notable: results }));
-            }
+            // console.log('Fetched results:', results[0].name, results[1].name);
+
+            // Simplified state update
+            setCharacterData(prevData => ({
+                ...prevData,
+                [charType]: results // Dynamically updating based on charType
+            }));
+
             toggleShowNames(cardId);
         } catch (error) {
             console.error('There was a problem with the fetch operation:', error);
-            setCharacterData([]);
+            setCharacterData(prevData => ({ ...prevData, [charType]: [] })); // Reset only the problematic part
         }
     };
 
@@ -35,5 +39,6 @@ export default function useCharacterDataFetcher() {
             [id]: !prevData[id]
         }));
     };
+
     return { characterData, showNames, fetchData, toggleShowNames };
-};
+}
