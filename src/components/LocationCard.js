98fa73f8878c3e5c9059fde57db314f9aff7375c
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-06-21 11:22:43 -0400

    destructure

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -2,36 +2,37 @@ import React from 'react';
 import { Card, CardContent, Typography, Button, CardActions } from '@mui/material';
 
 function LocationCard({ data }) {
+  const {id, name, territory, region, debut, notable_inhabitants, notable_former_inhabitants} = data;
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {data.name}
+          {name}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Territory: {data.territory}
+          Territory: {territory}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Region: {data.region}
+          Region: {region}
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Debut: {data.debut}
+          Debut: {debut}
         </Typography>
-        {data.notable_inhabitants && data.notable_inhabitants.length > 0 && (
+        {notable_inhabitants && notable_inhabitants.length > 0 && (
           <>
             <Typography variant="body2" color="text.secondary">Notable Inhabitants:</Typography>
             <ul>
-              {data.notable_inhabitants.map((inhabitant, idx) => (
+              {notable_inhabitants.map((inhabitant, idx) => (
                 <li key={idx}>{inhabitant}</li>
               ))}
             </ul>
           </>
         )}
-        {data.notable_former_inhabitants && data.notable_former_inhabitants.length > 0 && (
+        {notable_former_inhabitants && notable_former_inhabitants.length > 0 && (
           <>
             <Typography variant="body2" color="text.secondary">Notable Former Inhabitants:</Typography>
             <ul>
-              {data.notable_former_inhabitants.map((inhabitant, idx) => (
+              {notable_former_inhabitants.map((inhabitant, idx) => (
                 <li key={idx}>{inhabitant}</li>
               ))}
             </ul>
@@ -39,7 +40,7 @@ function LocationCard({ data }) {
         )}
       </CardContent>
       <CardActions>
-        <Button onClick={() => console.log('More about:', data.name)} size="large">Learn More</Button>
+        <Button onClick={() => console.log('More about:', name)} size="large">Learn More</Button>
       </CardActions>
     </Card>
   );
