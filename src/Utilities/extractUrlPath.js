50641e61a263e9a44afa26d63a56ecadbccbab6c
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-07-08 18:42:21 -0400

    extract url path

---
+++ src/Utilities/extractUrlPath.js
@@ -0,0 +1,24 @@
+  // Function to extract URL path from a full URL
+  export const extractUrlPath = (url) => {
+    if (url && typeof url === 'string' && url.startsWith('https')) {
+      const index = url.indexOf(".com/") + 5;
+      return url.substring(index);
+    }
+    return null;
+  };
+
+
+
+
+
+
+
+// For more complex / custom url extraction
+
+  // export const extractUrlPathComplex = (url, desiredValue, startsWith, theIndexOf, indexNum) => {
+  //   if (url && typeof url === desiredValue && url.startsWith(startsWith)) {
+  //     const index = url.indexOf(".com/") + 5;
+  //     return url.substring(index);
+  //   }
+  //   return null;
+  // };
