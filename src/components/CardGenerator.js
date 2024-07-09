20beb0f1a995dd2c505eecf2c8481b39ffc9a66a
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-07-09 13:53:59 -0400

    seperate components

--- src/components/CardGenerator.js
+++ src/components/CardGenerator.js
@@ -1,32 +1,52 @@
-import React, { useCallback, useMemo } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useCallback, useMemo, memo } from 'react';
+import LoadingSpinner from './LoadingSpinner';
+import ErrorComponent from './ErrorComponent';
 import CharacterCard from './CharacterCard';
 import TitanCard from './TitanCard';
 import LocationCard from './LocationCard';
 import OrganizationCard from './OrganizationCard';
 import EpisodeCard from './EpisodeCard';
+import useGeneralDataFetch from '../hooks/useGeneralDataFetch';
+import { useCardContext } from '../contexts/CardContext';
+import { Grid } from '@mui/material';
 
-const CardGenerator = ({ data, type }) => {
-  const cardComponents = useMemo(() => ({
-    characters: CharacterCard,
-    locations: LocationCard,
-    organizations: OrganizationCard,
-    titans: TitanCard,
-    episodes: EpisodeCard,
-  }), []);
+const CardGenerator = memo(function App() {
+    const { currentType } = useCardContext();
+    const { generalApiData, isLoading, isError, fetchGeneralApiData } = useGeneralDataFetch();
 
-  const getCardComponent = useCallback((data, type) => {
-    console.log('getCardComponent render');
-    const Component = cardComponents[type];
-    return Component ? <Component data={data} /> : null;
-  }, [cardComponents]);
+    useEffect(() => {
+        if (currentType) {
+            fetchGeneralApiData(currentType);
+        }
+    }, [currentType, fetchGeneralApiData]);
 
-  return getCardComponent(data, type);
-};
 
-CardGenerator.propTypes = {
-  data: PropTypes.object.isRequired,
-  type: PropTypes.string.isRequired,
-};
+    const cardComponents = useMemo(() => ({
+        characters: CharacterCard,
+        locations: LocationCard,
+        organizations: OrganizationCard,
+        titans: TitanCard,
+        episodes: EpisodeCard,
+    }), []);
+
+    const getCardComponent = useCallback((data, type) => {
+        console.log('getCardComponent called');
+        const Component = cardComponents[type];
+        return Component ? <Component data={data} /> : null;
+    }, [cardComponents]);
+
+    if (isLoading) return <LoadingSpinner className="spinner-container" />;
+    if (isError) return <ErrorComponent message={isError} />;
+
+    return (
+        <Grid container>
+            {generalApiData.results && generalApiData.results.map((data) => (
+                <Grid item xs={12} sm={6} md={6} key={data.id}>
+                    {getCardComponent(data, currentType)}
+                </Grid>
+            ))}
+        </Grid>
+    );
+});
 
 export default CardGenerator;
---
+++ src/components/CardPicker.js
@@ -0,0 +1,28 @@
+import React, { useCallback, memo } from 'react';
+import { useCardContext } from '../contexts/CardContext';
+import {
+    ButtonContainer,
+    StyledButton
+} from '../styles/StyledComponents';
+
+const CardPicker = memo(function App() {
+    console.log('CARDPICKER rendered');
+    const { setCurrentType } = useCardContext();
+    const handleClick = useCallback((type) => {
+        setCurrentType(type);
+    }, []);
+
+    return (
+        <>
+            <ButtonContainer maxWidth="md" className="btn-container">
+                <StyledButton variant="contained" onClick={() => handleClick('characters')}>Characters</StyledButton>
+                <StyledButton variant="contained" onClick={() => handleClick('locations')}>Locations</StyledButton>
+                <StyledButton variant="contained" onClick={() => handleClick('organizations')}>Organizations</StyledButton>
+                <StyledButton variant="contained" onClick={() => handleClick('titans')}>Titans</StyledButton>
+                <StyledButton variant="contained" onClick={() => handleClick('episodes')}>Episodes</StyledButton>
+            </ButtonContainer>
+        </>
+    );
+});
+
+export default CardPicker;
