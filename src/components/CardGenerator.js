ab260a214cbf753f16ee43b5b6c8ff6429a0b2bd
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-07-09 13:00:51 -0400

    Update CardGenerator.js

--- src/components/CardGenerator.js
+++ src/components/CardGenerator.js
@@ -1,73 +1,32 @@
-import React, { useEffect, useState, useCallback, useMemo, memo } from 'react';
-import { Grid } from '@mui/material';
-import LoadingSpinner from './components/LoadingSpinner';
-import ErrorComponent from './components/ErrorComponent';
-import CharacterCard from './components/CharacterCard';
-import TitanCard from './components/TitanCard';
-import LocationCard from './components/LocationCard';
-import OrganizationCard from './components/OrganizationCard';
-import EpisodeCard from './components/EpisodeCard';
-import useGeneralDataFetch from './hooks/useGeneralDataFetch';
-import {
-    StyledContainer,
-    StyledPaper,
-    ButtonContainer,
-    StyledButton
-} from './styles/StyledComponents';
+import React, { useCallback, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import CharacterCard from './CharacterCard';
+import TitanCard from './TitanCard';
+import LocationCard from './LocationCard';
+import OrganizationCard from './OrganizationCard';
+import EpisodeCard from './EpisodeCard';
 
-const CardPickerButtons = memo(function App() {
-    console.log('App rendered');
+const CardGenerator = ({ data, type }) => {
+  const cardComponents = useMemo(() => ({
+    characters: CharacterCard,
+    locations: LocationCard,
+    organizations: OrganizationCard,
+    titans: TitanCard,
+    episodes: EpisodeCard,
+  }), []);
 
-    const { generalApiData, isLoading, isError, fetchGeneralApiData } = useGeneralDataFetch();
-    const [currentType, setCurrentType] = useState('');
+  const getCardComponent = useCallback((data, type) => {
+    console.log('getCardComponent render');
+    const Component = cardComponents[type];
+    return Component ? <Component data={data} /> : null;
+  }, [cardComponents]);
 
-    useEffect(() => {
-        if (currentType) {
-            console.log(`Fetching data for type: ${currentType}`);
-            fetchGeneralApiData(currentType);
-        }
-    }, [currentType, fetchGeneralApiData]);
+  return getCardComponent(data, type);
+};
 
-    const handleClick = useCallback((type) => {
-        console.log(`Button clicked for type: ${type}`);
-        setCurrentType(type);
-    }, []);
+CardGenerator.propTypes = {
+  data: PropTypes.object.isRequired,
+  type: PropTypes.string.isRequired,
+};
 
-    const cardComponents = useMemo(() => ({
-        characters: CharacterCard,
-        locations: LocationCard,
-        organizations: OrganizationCard,
-        titans: TitanCard,
-        episodes: EpisodeCard,
-    }), []);
-
-    const getCardComponent = useCallback((data, type) => {
-        console.log('getCardComponent called');
-        const Component = cardComponents[type];
-        return Component ? <Component data={data} /> : null;
-    }, [cardComponents]);
-
-    if (isLoading) return <LoadingSpinner className="spinner-container" />;
-    if (isError) return <ErrorComponent message={isError} />;
-
-    return (
-        <>
-            <ButtonContainer maxWidth="md" className="btn-container">
-                <StyledButton variant="contained" onClick={() => handleClick('characters')}>Characters</StyledButton>
-                <StyledButton variant="contained" onClick={() => handleClick('locations')}>Locations</StyledButton>
-                <StyledButton variant="contained" onClick={() => handleClick('organizations')}>Organizations</StyledButton>
-                <StyledButton variant="contained" onClick={() => handleClick('titans')}>Titans</StyledButton>
-                <StyledButton variant="contained" onClick={() => handleClick('episodes')}>Episodes</StyledButton>
-            </ButtonContainer>
-            <Grid container>
-                {generalApiData.results && generalApiData.results.map((data) => (
-                    <Grid item xs={12} sm={6} md={6} key={data.id}>
-                        {getCardComponent(data, currentType)}
-                    </Grid>
-                ))}
-            </Grid>
-        </>
-    );
-});
-
-export default CardPickerButtons;
+export default CardGenerator;
