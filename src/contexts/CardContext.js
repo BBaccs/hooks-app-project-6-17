cbb64c4e10c6ac31a7bb9ef5ee20b0c66cf17d10
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-07-15 20:02:19 -0400

    stash

--- src/contexts/CardContext.js
+++ src/contexts/CardContext.js
@@ -1,19 +1,28 @@
-import React, { createContext, useState, useContext, useMemo } from 'react';
+import React, { createContext, useContext, useState, useMemo } from 'react';
 
 const CardContext = createContext();
 
-export const CardProvider = ({ children }) => {
-  const [currentType, setCurrentType] = useState('');
-
-  const value = useMemo(() => ({ currentType, setCurrentType }), [currentType]);
-
-  return (
-    <CardContext.Provider value={value}>
-      {children}
-    </CardContext.Provider>
-  );
+export const useCardContext = () => {
+    return useContext(CardContext);
 };
 
-export const useCardContext = () => {
-  return useContext(CardContext);
+export const CardProvider = ({ children }) => {
+    const [currentType, setCurrentType] = useState('');
+    const [generalCache, setGeneralCache] = useState({});
+    const [episodeCache, setEpisodeCache] = useState({});
+
+    const value = useMemo(() => ({
+        currentType,
+        setCurrentType,
+        generalCache,
+        setGeneralCache,
+        episodeCache,
+        setEpisodeCache
+    }), [currentType, generalCache, episodeCache]);
+
+    return (
+        <CardContext.Provider value={value}>
+            {children}
+        </CardContext.Provider>
+    );
 };
--- src/hooks/useGeneralDataFetch.js
+++ src/hooks/useGeneralDataFetch.js
@@ -1,16 +1,17 @@
-import { useState, useCallback, useRef } from 'react';
+import { useState, useCallback, useContext } from 'react';
+import { useCardContext } from '../contexts/CardContext';
 
 export default function useGeneralDataFetch() {
+    const { generalCache, setGeneralCache, episodeCache, setEpisodeCache } = useCardContext();
     const [generalApiData, setApiData] = useState({ results: [] });
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setError] = useState(null);
-    const generalCache = useRef({});
 
     const fetchGeneralApiData = useCallback(async (type) => {
         console.log('Fetching general data for type:', type);
-        if (generalCache.current[type]) {
+        if (generalCache[type]) {
             console.log('Using cached general data for:', type);
-            setApiData(generalCache.current[type]);
+            setApiData(generalCache[type]);
             return;
         }
 
@@ -24,9 +25,12 @@ export default function useGeneralDataFetch() {
                     throw new Error('Network response was not ok ' + response.statusText);
                 }
                 const apiData = await response.json();
-                generalCache.current[type] = apiData; // Cache the response
+                setGeneralCache((prevCache) => ({
+                    ...prevCache,
+                    [type]: apiData
+                }));
                 setApiData(apiData);
-                console.log('Fetched general data:', generalCache.current);
+                console.log('Fetched general data:', apiData);
             } catch (error) {
                 setError(error.message);
                 console.error('There was a problem with the fetch operation:', error);
@@ -36,23 +40,19 @@ export default function useGeneralDataFetch() {
         } else {
             console.error('Invalid type provided to fetchGeneralApiData from AOT API:', type);
         }
-    }, []);
+    }, [generalCache, setGeneralCache]);
 
     const [episodeData, setEpisodeData] = useState({ results: [] });
     const [isEpisodeLoading, setIsEpisodeLoading] = useState(false);
     const [isEpisodeError, setEpisodeError] = useState(null);
-    const episodeCache = useRef({});
 
     const fetchEpisodeData = useCallback(async (episodeId) => {
         console.log('Fetching episode data for:', episodeId);
         const cacheKey = `episodes/${episodeId}`;
 
-        // Log the entire cache to verify the structure
-        console.log('Current episode cache:', episodeCache.current);
-
-        if (episodeCache.current[cacheKey]) {
+        if (episodeCache[cacheKey]) {
             console.log('Using cached episode data for:', cacheKey);
-            setEpisodeData(episodeCache.current[cacheKey]);
+            setEpisodeData(episodeCache[cacheKey]);
             return;
         }
 
@@ -66,7 +66,10 @@ export default function useGeneralDataFetch() {
                     throw new Error('Network response was not ok ' + response.statusText);
                 }
                 const apiData = await response.json();
-                episodeCache.current[cacheKey] = apiData; // Cache the response
+                setEpisodeCache((prevCache) => ({
+                    ...prevCache,
+                    [cacheKey]: apiData
+                }));
                 setEpisodeData(apiData);
                 console.log('Fetched and cached episode data:', apiData);
             } catch (error) {
@@ -78,7 +81,7 @@ export default function useGeneralDataFetch() {
         } else {
             console.error('Invalid episode ID provided to fetchEpisodeData from AOT API:', episodeId);
         }
-    }, []);
+    }, [episodeCache, setEpisodeCache]);
 
     return { 
         generalApiData, 
