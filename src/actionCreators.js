e2a3f7c5e77d452069db66a5da885e54d938fbd6
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2024-07-17 13:31:29 -0400

    commit

---
+++ src/actionCreators.js
@@ -0,0 +1,10 @@
+// Define action types
+const actionTypes = {
+    SET_ACTIVE_CARD: 'SET_ACTIVE_CARD'
+};
+
+// Define initial state
+const initialState = {
+    activeCardType: 'CHARACTERS',
+    data: null
+};
--- src/reducers.js
+++ src/reducers.js
@@ -1,18 +1,29 @@
 import { combineReducers } from 'redux';
+import LoadingSpinner from './LoadingSpinner';
+import ErrorComponent from './ErrorComponent';
+import CharacterCard from './CharacterCard';
+import TitanCard from './TitanCard';
+import LocationCard from './LocationCard';
+import OrganizationCard from './OrganizationCard';
+import EpisodeCard from './EpisodeCard';
+import { SET_ACTIVE_CARD } from './actionCreators';
 
-// Example slice reducer
-const exampleReducer = (state = {}, action) => {
-  switch (action.type) {
-    case 'EXAMPLE_ACTION':
-      return { ...state, example: action.payload };
-    default:
-      return state;
-  }
+const buttonReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.SET_ACTIVE_CARD:
+            return {
+                ...state,
+                activeCardType: action.payload.cardType,
+                data: action.payload.data
+            };
+        default:
+            return state;
+    }
 };
 
+
 const rootReducer = combineReducers({
-  example: exampleReducer,
-  // Add other reducers here
+    example: exampleReducer,
 });
 
 export default rootReducer;
